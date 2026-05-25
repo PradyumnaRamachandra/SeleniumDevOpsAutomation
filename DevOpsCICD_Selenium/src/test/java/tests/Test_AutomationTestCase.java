6cc27d954c39d416ab42d0239c2fdbd869479bb4
PradyumnaRamachandra <prady07@gmail.com>
2026-05-26 00:00:57 +0530

    Added TC01

---
+++ DevOpsCICD_Selenium/src/test/java/tests/Test_AutomationTestCase.java
@@ -0,0 +1,24 @@
+package tests;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test_AutomationTestCase {
+	
+	
+	@Test
+	public void TC01(){
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in");
+		driver.close();
+		
+	}
+
+}
